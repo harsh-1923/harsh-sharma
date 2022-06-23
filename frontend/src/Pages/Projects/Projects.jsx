2c590d63643fd5c120d7dc92875ff875ecb7d4f3
Harsh Sharma <harsh.ju.sharma@gmail.com>
2022-06-23 21:36:45 +0530

    changes

--- frontend/src/Pages/Projects/Projects.jsx
+++ frontend/src/Pages/Projects/Projects.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./Projects.css";
 
 const Projects = () => {
-  return <div className="project-wrapper">
-    
-    <h1 className="title">Projects</h1>
-  </div>;
+  return (
+    <div className="project-wrapper">
+      <h1 className="title">Projects</h1>
+    </div>
+  );
 };
 
 export default Projects;
